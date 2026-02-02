8b604a124bfee095cc1e12aa3ca29ea61b869ab8
JOMANJOLAN <14324630+jomanjolan@user.noreply.gitee.com>
2026-02-02 13:07:38 +0800

    重新格式化C/C++文件

--- ppmdemo/c/board.c
+++ ppmdemo/c/board.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     char buf[256];
     int fps = 60;
     int w = 16 * fps;
     int h = 9 * fps;
-    for (int i = 0; i < fps; i++)
-    {
+    for (int i = 0; i < fps; i++) {
         snprintf(buf, sizeof(buf), "output_%02d.ppm", i);
-        const char *output_path = buf;
-        FILE *file = fopen(output_path, "wb");
-        fprintf(file, "P6\n");          // format
-        fprintf(file, "%d %d\n", w, h); // width / height
-        fprintf(file, "255\n");         // the maximum value of the color component, usually 255 for P6
-        for (int y = 0; y < h; y++)
-        {
-            for (int x = 0; x < w; x++)
-            {
-                if (((x + i) / fps + (y + i) / fps) % 2)
+        const char* output_path = buf;
+        FILE* file = fopen(output_path, "wb");
+        fprintf(file, "P6\n");           // format
+        fprintf(file, "%d %d\n", w, h);  // width / height
+        fprintf(file, "255\n");          // the maximum value of the color component, usually 255 for P6
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                if (((x + i) / fps + (y + i) / fps) % 2) {
                     fputc(0xFF, file);
-                else
+                } else {
                     fputc(0x00, file);
+                }
                 fputc(0x00, file);
                 fputc(0x00, file);
             }
